84ad84448a262aa9538148f749a7494bb1df55c4
Pierre <pierre.decrock@gmail.com>
2021-04-20 18:12:20 +0200

    story1 update

--- generation.sh
+++ generation.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+# set -x
 
+if [[ $# -eq 1 ]]; then
+    delay=$1
+    verbose_mode=false
+elif [[ $# -eq 2 && $1 == "-v" || $1 == "--verbose" ]]; then
+    delay=$2
+    verbose_mode=true
+else
+    echo "Invalid arguments"
+    exit 1
+fi
 echo "Hello world!"
 
 # Compile genTick.c
-gcc -Wall -o genTick genTick.c
+# gcc -Wall -o genTick genTick.c
 
 # Run genTick.o with delay argument (millisecond)
-./genTick 1000
+./genTick $delay
 
