79d48cf7c449600f6d391ba3b4941337645e3bc9
etienne <etienne.buronfosse@edu.itescia.fr>
2021-04-21 14:21:24 +0200

    putain

--- generation.sh
+++ generation.sh
@@ -14,7 +14,7 @@ fi
 echo "Hello world!"
 
 # Compile genTick.c
-# gcc -Wall -o genTick genTick.c
+gcc -Wall -o genTick genTick.c
 
 # Run genTick.o with delay argument (millisecond)
 ./genTick $delay
